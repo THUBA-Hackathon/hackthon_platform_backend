4993575cd72958210fc1c67575a54aedeacd0b96
Xavier-YangCN <38470282+Xavier-YangCN@users.noreply.github.com>
2021-12-07 13:19:51 +0800

    hackthon part finished

--- src/hackthon_platform/src/lib.rs
+++ src/hackthon_platform/src/lib.rs
@@ -1,14 +1,26 @@
-use time::*;
+use ic_cdk::export::{candid::{CandidType, Deserialize}};
+use ic_cdk::storage;
+use ic_cdk_macros::*;
 
+
+type HackthonStore = Vec<Hackthon>;
+
+#[derive(Clone, Default, CandidType, Debug, Deserialize)]
 struct Hackthon {
-    title: String,
-    tags: [String],
-    holding_time: PrimitiveDateTime, 
+    pub title: String,
+    pub tags: Vec<String>,
+    pub holding_time: String,
+    pub intro: String,
 }
 
+#[update(name = addHackthon)]
+fn add_hackthon(hackthon_info: Hackthon) {
+    let hackthon_store = storage::get_mut::<HackthonStore>();
+    hackthon_store.push(hackthon_info);
+}
 
-#[ic_cdk_macros::query]
-fn print() {
-    ic_cdk::print("Hello World from DFINITY!");
-    
+#[query(name = listHackthon)]
+fn list_hackthon() -> &'static Vec<Hackthon>{
+    let hackthon_store = storage::get::<HackthonStore>();
+    return hackthon_store;
 }
