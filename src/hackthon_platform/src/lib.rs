b5cd0aefbe1fae605ed3c1e2de1e36e6edb02a61
Xavier-YangCN <38470282+Xavier-YangCN@users.noreply.github.com>
2021-12-07 15:59:46 +0800

    Group data structure

--- src/hackthon_platform/src/lib.rs
+++ src/hackthon_platform/src/lib.rs
@@ -1,4 +1,4 @@
-use ic_cdk::export::{candid::{CandidType, Deserialize}};
+use ic_cdk::export::{candid::{CandidType, Deserialize, Nat}};
 use ic_cdk::storage;
 use ic_cdk_macros::*;
 
@@ -9,9 +9,31 @@ type HackthonStore = Vec<Hackthon>;
 struct Hackthon {
     pub title: String,
     pub tags: Vec<String>,
-    pub holding_time: String,
+    pub ddl: String,
+    pub intro: String,
+    pub publish_time: String,
+}
+
+enum State {
+    Student,
+    Worker,
+}
+
+struct Person {
+    pub name: String,
+    pub tech: Vec<String>,
+    pub state: State,
+    pub email: String,
+    pub phone: String,
+    pub wechat: String,
+}
+
+struct Group {
+    pub name: String,
+    pub number: String,
     pub intro: String,
 }
+
 
 #[update(name = addHackthon)]
 fn add_hackthon(hackthon_info: Hackthon) {
@@ -19,8 +41,41 @@ fn add_hackthon(hackthon_info: Hackthon) {
     hackthon_store.push(hackthon_info);
 }
 
-#[query(name = listHackthon)]
-fn list_hackthon() -> &'static Vec<Hackthon>{
-    let hackthon_store = storage::get::<HackthonStore>();
-    return hackthon_store;
+
+
+#[query(name = listHackthonByPub)]
+fn list_hackthon_by_publish_time() -> &'static Vec<Hackthon>{
+    let hackthon_store = storage::get_mut::<HackthonStore>();
+    hackthon_store.sort_by(|a, b| 
+                            b.publish_time.cmp(&a.publish_time));
+    hackthon_store
 }
+
+#[query(name = listHackthonByDDL)]
+fn list_hackthon_by_ddl() -> &'static Vec<Hackthon> {
+    let hackthon_store = storage::get_mut::<HackthonStore>();
+    hackthon_store.sort_by(|a, b| 
+        b.ddl.cmp(&a.ddl));
+    hackthon_store
+}
+
+#[query(name = listHackthonByTag)]
+fn list_hackthon_by_tag() -> &'static Vec<Hackthon> {
+    let hackthon_store = storage::get_mut::<HackthonStore>();
+    hackthon_store.sort_by(|a, b| 
+        b.tags.cmp(&a.tags));
+    hackthon_store
+}
+
+#[query(name = searchHackthonByTag)]
+fn search_hackthon_by_tag(tag: String) -> Vec<Hackthon>  {
+    let hackthon_store = storage::get::<HackthonStore>();
+    let mut search_result = Vec::new();
+    for h in hackthon_store.iter() {
+        if h.tags.contains(&tag) {
+            search_result.push(h.clone());
+        }
+    }
+    search_result
+}
+
