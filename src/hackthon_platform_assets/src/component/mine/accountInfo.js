9aeade2ea908b47c22bf91b48b5935948581b38a
miracleXH <h1047741619@163.com>
2022-01-04 22:33:56 +0800

    add submit userinfo

--- src/hackthon_platform_assets/src/component/mine/accountInfo.js
+++ src/hackthon_platform_assets/src/component/mine/accountInfo.js
@@ -4,8 +4,18 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import './mine.css'
 import { hackthon_platform } from "../../../../declarations/hackthon_platform";
+import { AuthClient } from "@dfinity/auth-client";
+
+const days = BigInt(1);
+const hours = BigInt(24);
+const nanoseconds = BigInt(3600000000000);
 
 var usr_addr = localStorage.getItem('id');
+async function handleAuthenticated(authClient) {
+  const identity = await authClient.getIdentity();
+  window.localStorage.setItem("id", identity.getPrincipal().toString());
+  
+}
 // 需要传入 用户名称 用户国家 手机 邮箱 希望担任的角色
 export default function AccountInfo() {
   const [name, setName] = React.useState('')
@@ -13,21 +23,49 @@ export default function AccountInfo() {
   const [phone, setPhone] = React.useState('')
   const [email, setEmail] = React.useState('')
   const [skills, setSkills] = React.useState('')
+  const [school, setSchool] = React.useState('')
 
   const handleSubmit = async () => {
     // 提交表单
-    var skill_list = skills.split(" ");
-    await hackthon_platform.createUserInfo({id : usr_addr, school: 'test' ,name : name, area : area, phone : phone, email : email, skills : skill_list});
-    console.log("aaaaaaatest")
+    console.log(usr_addr)
+    if(!usr_addr) {
+      const authClient = await AuthClient.create();
+        if (await authClient.isAuthenticated()) {
+            handleAuthenticated(authClient);
+        }
+        authClient.login({
+            onSuccess: async () => {
+                // authClient now has an identity
+                handleAuthenticated(authClient);
+            },
+            identityProvider:"https://identity.ic0.app/#authorize",
+            // Maximum authorization expiration is 8 days
+            maxTimeToLive: days * hours * nanoseconds,
+        });
+    }
+    else {
+      var skill_list = skills.split(",");
+      console.log(usr_addr + school + name + phone + email + skills + area)
+      console.log(skill_list)
+      await hackthon_platform.createUserInfo({id : usr_addr, school: school ,name : name, area : area, phone : phone, email : email, skills : skill_list});
+
+      let sleep= (time)=> new Promise((resolve)=>{
+        setTimeout(resolve,time)
+      })
+      // sleep(2000);
+      // window.location.reload()
+      console.log("finish create userinfo")
+    }
+    
   };
 
-  React.useEffect( async ()=>{
-    var usr_addr = localStorage.getItem('id');
-    console.log(typeof(usr_addr))
+  React.useEffect(async ()=>{
     try{
-      var userInfo = await hackthon_platform.getUserInfo({id : usr_addr});
+      var userInfo = await hackthon_platform.getUserInfo(usr_addr);
+      console.log(userInfo)
       setName(userInfo.name)
       setArea(userInfo.area)
+      setSchool(userInfo.school)
       setPhone(userInfo.phone)
       setEmail(userInfo.email)
       setSkills(userInfo.skills)
@@ -45,7 +83,7 @@ export default function AccountInfo() {
                 label="昵称"
                 fullWidth
                 variant="outlined"
-                defaultValue={name}
+                value={name}
                 onChange={(nameValue) => {setName(nameValue.target.value)}}
             />
             <TextField
@@ -54,8 +92,17 @@ export default function AccountInfo() {
                 label="来自国家/地区"
                 fullWidth
                 variant="outlined"
-                defaultValue={area}
+                value={area}
                 onChange={(areaValue) => {setArea(areaValue.target.value)}}
+            />
+            <TextField
+                margin="dense"
+                id="area"
+                label="学校"
+                fullWidth
+                variant="outlined"
+                value={school}
+                onChange={(schoolValue) => {setSchool(schoolValue.target.value)}}
             />
             <TextField
                 margin="dense"
@@ -63,7 +110,7 @@ export default function AccountInfo() {
                 label="手机"
                 fullWidth
                 variant="outlined"
-                defaultValue={phone}
+                value={phone}
                 onChange={(phoneValue) => {setPhone(phoneValue.target.value)}}
             />
             <TextField
@@ -73,7 +120,7 @@ export default function AccountInfo() {
                 variant="outlined"
                 fullWidth
                 type="email"
-                defaultValue={email}
+                value={email}
                 onChange={(emailValue) => {setEmail(emailValue.target.value)}}
             />
             <TextField
@@ -83,7 +130,7 @@ export default function AccountInfo() {
                 fullWidth
                 variant="outlined"
                 multiline
-                defaultValue={skills}
+                value={skills}
                 onChange={(skillsValue) => {setSkills(skillsValue.target.value)}}
             />
             {/* <Button onClick={handleClose}>取消</Button> */}
