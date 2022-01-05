f820120c1d144113747a8081fb71b49e9da63197
Xavier-YangCN <38470282+Xavier-YangCN@users.noreply.github.com>
2022-01-05 17:26:22 +0800

    updated backend

--- src/hackthon_platform/demo.sh
+++ src/hackthon_platform/demo.sh
@@ -1,7 +1,7 @@
 sudo dfx deploy
 
-sudo dfx canister  call hackthon_platform createUserInfo  '(record {id = "aa"; name = "alice"; area = "china"; phone = "110"; email = "alice@gmail.com"; school = "pku"; skills = vec {"js"}})'
-sudo dfx canister  call hackthon_platform getUserInfo '("aa")'
+sudo dfx canister  call hackthon_platform createUserInfo  '(record {name = "alice"; area = "china"; phone = "110"; email = "alice@gmail.com"; school = "pku"; skills = vec {"js"}})'
+sudo dfx canister  call hackthon_platform getSelfUserInfo
 sudo dfx canister call hackthon_platform createHackathon '(record {id = "bb"; name = "testHackathon"; intro="Ahahhh"; sponsor= "Boy"; startdate="2021"; enddate="2022"; teams= vec{"none"}})'
 sudo dfx canister call hackthon_platform getHackathonList
 sudo dfx canister call hackthon_platform createTeam '(record {id = "cc"; hackathon_id = "bb"; name = "testTeam"; intro = "shit"; members = vec{"aa"}; skills_needed = vec{"Rust";"Frontend"}; code_link = ""; video_link = ""})'
