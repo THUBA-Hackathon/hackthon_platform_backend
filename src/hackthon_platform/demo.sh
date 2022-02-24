6686822afb73f9f531737e92e311c1b59c287301
Xavier-YangCN <38470282+Xavier-YangCN@users.noreply.github.com>
2022-02-24 09:46:02 +0800

    update demo.sh

--- src/hackthon_platform/demo.sh
+++ src/hackthon_platform/demo.sh
@@ -4,12 +4,7 @@ sudo dfx canister  call hackthon_platform createUserInfo  '(record {name = "alic
 sudo dfx canister  call hackthon_platform getSelfUserInfo
 sudo dfx canister call hackthon_platform createHackathon '(record {id = "bb"; name = "testHackathon"; intro="Ahahhh"; sponsor= "Boy"; startdate="2021"; enddate="2022"; teams= vec{"none"}})'
 sudo dfx canister call hackthon_platform getHackathonList
-sudo dfx canister call hackthon_platform createTeam '(record {id = "cc"; hackathon_id = "bb"; name = "testTeam"; intro = "shit"; members = vec{"aa"}; skills_needed = vec{"Rust";"Frontend"}; code_link = ""; video_link = ""})'
+sudo dfx canister call hackthon_platform createTeam '(record {id = "cc"; hackathon_id = "bb"; name = "testTeam"; intro = "shit"; members = vec{}; skills_needed = vec{"Rust";"Frontend"}; code_link = ""; video_link = ""})'
 sudo dfx canister call hackthon_platform getHackathonList
 sudo dfx canister call hackthon_platform getTeamMembers '("cc")'
-sudo dfx canister  call hackthon_platform createUserInfo  '(record {id = "dd"; name = "bob"; area = "china"; phone = "110"; email = "alice@gmail.com"; school = "pku"; skills = vec {"rust"}})'
-sudo dfx canister  call hackthon_platform joinTeam '("dd", "cc")'
 sudo dfx canister call hackthon_platform getTeamList '("bb")'
-sudo dfx canister call hackthon_platform getMessage '("aa")'
-sudo dfx canister call hackthon_platform applyMessage '("0", true)'
-sudo dfx canister call hackthon_platform getAllMessage
