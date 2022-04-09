3d32b25765cce2b27fa962318b995515ea5200d3
DhunterAO <DhunterAO@users.noreply.github.com>
2022-04-09 18:37:07 +0800

    update whitelist

--- src/hackthon_platform_assets/src/component/createHackathonUserID.js
+++ src/hackthon_platform_assets/src/component/createHackathonUserID.js
@@ -1,10 +1,7 @@
 // 此处添加可以创建黑客松的白名单id
 export const CREATE_HACKATHON_USER = [
-    "z2ypx-3zr5b-ocpje-wruch-2azmi-pfpl6-ghdc2-g3zhl-3dcwa-zsa6r-3ae",
-    "qmkmb-2k4p3-chw76-nd7bu-qqxwg-d3u3s-3p5ev-5oxk6-hkwf2-4d4gb-pae",
-    "xysqw-zrknq-575sn-w3ymw-35dei-hr5vb-5ud7b-sxtkx-penhp-w5nna-fae",
-    "wn2fq-v47jv-hrydn-v5hea-pcpbc-nuemw-4g5wb-qecas-bxqv3-xzc3w-7ae",
-    "oixhp-z3mef-rkr2e-4yfan-46jc7-6sji2-ulxz5-javyd-uga3l-dq3sh-nae",
-    "5aslw-f3cet-fakwv-7hs7s-3qxjt-tavec-ohgsm-orz5l-bpkmn-zinek-yqe",
-    "tw7ym-hlq6s-tdhhv-3ppd3-izvzm-xxkoo-rs4rb-klsew-lfrdb-w3xwx-rae"
+    "rv4ta-6izdj-7hnk2-qiq7p-2xx6z-76dhs-hc4zp-vmfrc-ququa-eawoi-eqe",
+    "f7sun-mczxy-vr3kg-rfroz-32edo-qnk77-2bsq7-hcpih-cmojt-k2hid-kae",
+    "57t3m-abt3g-3pj3l-h35kw-bglch-mbr3e-yjp5i-n4a7f-xe4cx-77zt4-cqe",
+    "hb4pp-vy36i-klh7t-ldesu-5m5lh-waldx-u4kct-uc35g-ewwsq-i4hop-rae",
 ]
